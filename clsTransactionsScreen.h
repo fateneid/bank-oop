85f19e4f2253de0622a087730e30f30efec3f0f8
Faten Eid <mulan.tech.914h@gmail.com>
2025-02-28 02:31:17 +0200

    Total Balances Screen

--- clsTransactionsScreen.h
+++ clsTransactionsScreen.h
@@ -5,6 +5,7 @@
 #include "clsInputValidate.h"
 #include "clsDepositScreen.h"
 #include "clsWithdrawScreen.h"
+#include "clsTotalBalancesScreen.h"
 using namespace std;
 
 class clsTransactionsScreen : protected clsScreen
@@ -40,7 +41,10 @@ private:
 
     static void _ShowTotalBalancesScreen()
     {
-        cout << "\n Balances Screen will be here.\n";
+        //cout << "\n Balances Screen will be here.\n";
+
+        clsTotalBalancesScreen::ShowTotalBalances();
+
     }
 
     static void _GoBackToTransactionsMenue()
